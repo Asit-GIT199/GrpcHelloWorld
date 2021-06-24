0c1fc579f22f6942f6bf7a13669c6de2435ba71d
Asit Kumar Mohanty <asit199@gmail.com>
2021-06-24 11:24:26 +0530

    Add project files.

---
+++ GrpcHelloWorld/Services/HelloWorldService.cs
@@ -0,0 +1,31 @@
+﻿using Grpc.Core;
+using GrpcHelloWorld.Protos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcHelloWorld.Services
+{
+    public class HelloWorldService : HelloService.HelloServiceBase
+    {
+        private readonly ILogger<HelloWorldService> _logger;
+
+        public HelloWorldService(ILogger<HelloWorldService> logger)
+        {
+            _logger = logger;
+        }
+        public override Task<HelloResponse> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            string resultMessage = $"Hello {request.Name}";
+
+            var response = new HelloResponse
+            {
+                Message = resultMessage
+            };
+
+            return Task.FromResult(response);
+        }
+    }
+}
