0a15626cf29879790d39174caa5e6935fc69cf84
Asit Kumar Mohanty <asit199@gmail.com>
2021-06-25 01:18:16 +0530

    Grpc template with Grpc reflection sample application.

---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "Swn Client" }
+                );
+
+            Console.WriteLine("Greetings: " + reply.Message);
+            Console.ReadKey();
+        }
+    }
+}
